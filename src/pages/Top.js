9d22839dfb4b67ef91999a959d33a78595103ec2
Avinash Kondareddy <akondare@ucsd.edu>
2018-08-31 01:37:40 -0700

    adds resume

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -6,6 +6,7 @@ import Switch from '@material-ui/core/Switch';
 import SvgIcon from '@material-ui/core/SvgIcon';
 import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
+import resume from '../assets/resume.pdf';
 
 
 function Top(props) {
@@ -33,7 +34,7 @@ function Top(props) {
             </IconButton>
             </Tooltip>
             <Tooltip title='Resume'>
-            <IconButton color='inherit' href="https://github.com/Avikndr">
+            <IconButton color='inherit' href={resume}>
                 <SvgIcon>
                     <path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z" />
                 </SvgIcon>
